f1f571068a79a4c040da1f9ce3c9ec56e984ba47
Arijit Bhatta <arijitbhatta123@gmail.com>
2022-12-19 17:20:16 +0530

    adding rsume to navbar

--- main.js
+++ main.js
@@ -14,6 +14,12 @@ document.querySelector('.btn').onclick = function(){
     let redirectWindow = window.open('https://drive.google.com/file/d/1C-xbWdSqu-T6EKS3FVgs7_7FcTr-q9VX/view','_blank');
     redirectWindow.location;
 }
+
+document.querySelector('.resume').onclick = function(){
+    let redirectWindow = window.open('https://drive.google.com/file/d/1C-xbWdSqu-T6EKS3FVgs7_7FcTr-q9VX/view','_blank');
+    redirectWindow.location;
+}
+
 // header color change
 
 // let header = document.querySelector('header')
